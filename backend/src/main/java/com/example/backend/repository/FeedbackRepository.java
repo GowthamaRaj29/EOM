be1c6ad6f1e46afa9d1a79d188a123a4024f554c
Gowthama_Raj <117590685+GowthamaRaj29@users.noreply.github.com>
2024-08-08 08:47:19 +0530

    Delete backend directory

--- backend/src/main/java/com/example/backend/repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.backend.model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
