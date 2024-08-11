65e5446eae3b0fdfe92d12b19f3d695e2e2d60c0
Gowthama_Raj <117590685+GowthamaRaj29@users.noreply.github.com>
2024-08-11 19:14:45 +0530

    Delete eomback directory

--- eomback/src/main/java/com/security/template/repo/PolicyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.security.template.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.security.template.model.Policy;
-
-public interface PolicyRepository extends JpaRepository<Policy, Integer> {
-}
